685e379ad40be14e636c501c5fae0d307fcbba99
alnuzailyezzuddeen-cyber <alnuzailyezzuddeen@gmail.com>
2026-08-03 01:20:22 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'dari_yemen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('تطبيق داري اليمن'),
+        ),
+        body: const Center(
+          child: Text(
+            'أهلاً بك في تطبيق داري اليمن',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
