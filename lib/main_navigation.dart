63d1c5ec34e5c4fb826d40c54c0c4bc64a584fb4
alnuzailyezzuddeen-cyber <alnuzailyezzuddeen@gmail.com>
2026-08-03 01:42:39 +0300

    Update main_navigation.dart
    import 'package:flutter/material.dart';
    
    class MainNavigation extends StatelessWidget {
      const MainNavigation({super.key});
    
      @override
      Widget build(BuildContext context) {
        return const Scaffold(
          body: Center(
            child: Text('Navigation Main Page'),
          ),
        );
      }
    }

--- lib/main_navigation.dart
+++ lib/main_navigation.dart
@@ -1,1 +1,15 @@
 
+import 'package:flutter/material.dart';
+
+class MainNavigation extends StatelessWidget {
+  const MainNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Navigation Main Page'),
+      ),
+    );
+  }
+}
