84c1625e63766b930f86e8b4030555db7527e8c4
alnuzailyezzuddeen-cyber <alnuzailyezzuddeen@gmail.com>
2026-08-03 01:45:13 +0300

    Update property_details_screen.dart
    import 'package:flutter/material.dart';
    
    class PropertyDetailsScreen extends StatelessWidget {
      const PropertyDetailsScreen({super.key});
    
      @override
      Widget build(BuildContext context) {
        return Scaffold(
          appBar: AppBar(
            title: const Text('تفاصيل العقار'),
          ),
          body: const Center(
            child: Text('صفحة تفاصيل العقار'),
          ),
        );
      }
    }

--- lib/property_details_screen.dart
+++ lib/property_details_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PropertyDetailsScreen extends StatelessWidget {
+  const PropertyDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('تفاصيل العقار'),
+      ),
+      body: const Center(
+        child: Text('صفحة تفاصيل العقار'),
+      ),
+    );
+  }
+}
 
